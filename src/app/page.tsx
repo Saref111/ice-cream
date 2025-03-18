ff766e944a25f08727a27a262801f4e310bd7a8d
Ivan Borovyk <saref012@gmail.com>
2025-03-18 22:33:17 +0200

    fixed use client error and add exaple mui buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import Button from '@mui/material/Button';
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -5,6 +8,12 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Hello World</h1>
+        <Button variant="contained" color="primary">
+          Click me
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Secondary Button
+        </Button>
       </main>
     </div>
   );
